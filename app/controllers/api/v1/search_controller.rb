144cc0673d38ef90d76cbe8a841cf35d86c43ee4
Murtaza Bagwala <bagwalamurtaza@gmail.com>
2022-11-27 11:58:58 +0530

    add basic controllers and services

---
+++ app/controllers/api/v1/search_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class SearchController < ApplicationController
+      def search
+        
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      get :search
+    end
+  end
 end
