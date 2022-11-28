a0e29334e0dd7f3019c1f91e8b25fb2ba4785630
Murtaza Bagwala <bagwalamurtaza@gmail.com>
2022-11-28 11:28:13 +0530

    add search method

--- app/models/trie/node.rb
+++ app/models/trie/node.rb
@@ -1,15 +1,31 @@
 
 class Node 
-  def initialize(children = {}, value) 
+  def initialize(frequency, value)
+    @frequency = frequency
+    @value = value 
     @children = initialize_children
-    @value = value
+    @prefixes = []
+    @is_last_node = false
   end
   
-  attr_accessor :children, :value, is_last_char
+  attr_accessor :children, :value, :is_last_node, :prefixes
+
+  def get_all_words
+    get_words(self)
+  end
 
   private
 
+  def get_words(child)
+
+    return [] if !child || child.is_last_node
+
+    child.children.values.map do |child|
+      get_words(child) << child.value
+    end.flatten
+  end
+
   def initialize_children
-    [].inject({}) { |object, value| object[value] = nil; object }
+    ALPHABETS.inject({}) { |object, value| object[value] = nil; object }
   end
 end
--- app/models/trie/tree.rb
+++ app/models/trie/tree.rb
@@ -3,16 +3,32 @@ class Tree
 
   def insert(key)
     current_node = root
+
     for index in [0..key.length - 1] do
       node = current_node.children[key[index]]
       if node == nil 
-        new_node = Node.new
+        new_node = Node.new(0, key[0..index])
         node.children[key[index]] = new_node
         current_node = new_node
       else
         current_node = node
       end
     end
+    current_node.is_last_node = true;
+  end 
+
+  def search(key)
+    current_node = root
+    for index in [0..key.length - 1] do
+      node = current_node.children[key[index]]
+      char = key[index]
+      if node == nil 
+        return []
+      else
+        current_node = node
+      end
+    end
+    return current_node.get_all_words
   end 
 
   def root
